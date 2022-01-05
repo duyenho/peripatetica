c0f3b10beca123a692ff41d2682a93f2f5e0f8b4
Duyen Ho <duyenho@gmail.com>
2022-01-06 00:06:22 +1100

    read csv data

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-import { Client } from '@googlemaps/google-maps-services-js';
+import fs from 'fs'
+import csv from 'csv-parser'
+// import { Client } from '@googlemaps/google-maps-services-js'
 
-const client = new Client({});
+// const client = new Client({})
 
-client
-  .geocode({
-    params: {
-      address: "London",
-      key: process.env.GOOGLE_MAPS_API_KEY
-    },
-    timeout: 1000
+fs.createReadStream('data/liam.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row)
   })
-  .then(r => {
-    console.log(r.data.results[0]);
-  })
-  .catch(e => {
-    console.log(e);
+  .on('end', () => {
+    console.log('CSV file successfully processed')
   });
+
+console.log('Fin');
+
+
+// client
+//   .geocode({
+//     params: {
+//       address: "London",
+//       key: process.env.GOOGLE_MAPS_API_KEY
+//     },
+//     timeout: 1000
+//   })
+//   .then(r => {
+//     console.log(r.data.results[0]);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
   
