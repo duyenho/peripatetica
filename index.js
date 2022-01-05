caa62e98aaa3a7a1e496dc894710323503e3eb3b
Duyen Ho <duyenho@gmail.com>
2022-01-06 01:10:40 +1100

    write geocodes to csv

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ import fs from 'fs'
 import csv from 'csv-parser'
 import { Client } from '@googlemaps/google-maps-services-js'
 
+function writeToCSVFile(cities) {
+  const filename = 'data/liam_geocodes.csv'
+
+  fs.writeFile(filename, extractAsCSV(cities), err => {
+    if (err) {
+      console.log('Error writing to CSV file', err)
+    } else {
+      console.log(`Saved as ${filename}`)
+    }
+  })
+}
+
+function extractAsCSV(cities) {
+  const header = ['city,date,duration,latitude,longitude'];
+  const rows = cities.map(city =>
+    `${city.city},${city.date},${city.duration},${city.latitude},${city.longitude}`
+  )
+  return header.concat(rows).join('\n')
+}
+
 const client = new Client({})
 const cities = []
 
@@ -25,13 +45,12 @@ fs.createReadStream('data/liam.csv')
           longitude: geocode.lng,
         }
         cities.push(city)
+        writeToCSVFile(cities)
       })
       .catch(e => {
         console.log(e);
       });
   })
   .on('end', () => {
-    console.log('CSV file successfully processed')
+    console.log('Fin');
   });
-
-console.log('Fin');
