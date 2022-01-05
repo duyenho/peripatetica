a31ee4434809187024b0cc1cf782607f9d41da78
Duyen Ho <duyenho@gmail.com>
2022-01-06 00:29:03 +1100

    get latitude and longitude for each city

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 import fs from 'fs'
 import csv from 'csv-parser'
-// import { Client } from '@googlemaps/google-maps-services-js'
+import { Client } from '@googlemaps/google-maps-services-js'
 
-// const client = new Client({})
+const client = new Client({})
+const cities = []
 
 fs.createReadStream('data/liam.csv')
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row)
+    client
+      .geocode({
+        params: {
+          address: row.city,
+          key: process.env.GOOGLE_MAPS_API_KEY
+        },
+      })
+      .then(r => {
+        const geocode = r.data.results[0].geometry.location
+        const city = {
+          city: row.city,
+          date: row.date,
+          duration: row.duration,
+          latitude: geocode.lat,
+          longitude: geocode.lng,
+        }
+        cities.push(city)
+      })
+      .catch(e => {
+        console.log(e);
+      });
   })
   .on('end', () => {
     console.log('CSV file successfully processed')
   });
 
 console.log('Fin');
-
-
-// client
-//   .geocode({
-//     params: {
-//       address: "London",
-//       key: process.env.GOOGLE_MAPS_API_KEY
-//     },
-//     timeout: 1000
-//   })
-//   .then(r => {
-//     console.log(r.data.results[0]);
-//   })
-//   .catch(e => {
-//     console.log(e);
-//   });
-  
