926830057e7493696d81485b30a93ccdc17af793
Duyen Ho <duyenho@gmail.com>
2022-01-05 22:22:51 +1100

    fetch geocode data from google maps api

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hai')
+import { Client } from '@googlemaps/google-maps-services-js';
+
+const client = new Client({});
+
+client
+  .geocode({
+    params: {
+      address: "London",
+      key: process.env.GOOGLE_MAPS_API_KEY
+    },
+    timeout: 1000
+  })
+  .then(r => {
+    console.log(r.data.results[0]);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+  
